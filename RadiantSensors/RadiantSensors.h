0f88b335c883d02dee03092918bbb6a975654993
Vahid <vahidgiga@gmail.com>
2017-01-29 10:11:12 +0330

    Initial Commit

---
+++ RadiantSensors/RadiantSensors.h
@@ -0,0 +1,19 @@
+//
+//  RadiantSensors.h
+//  RadiantSensors
+//
+//  Created by Vahid Sayad on 1/29/17.
+//  Copyright © 2017 DominateRFID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RadiantSensors.
+FOUNDATION_EXPORT double RadiantSensorsVersionNumber;
+
+//! Project version string for RadiantSensors.
+FOUNDATION_EXPORT const unsigned char RadiantSensorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RadiantSensors/PublicHeader.h>
+
+
